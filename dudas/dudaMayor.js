504ddfe8f6e70980d5f0e10e936fb5000e93940d
Johan <johan-avila@outlook.com>
2020-12-14 19:18:48 -0600

    Commit

--- dudas/dudaMayor.js
+++ dudas/dudaMayor.js
@@ -7,7 +7,7 @@ const getA = ()=>{
       cb("B")
     }, 10)
   }
-  const getC = ()=> { 
+  const getC = () => { 
     return new Promise((resolve,reject)=>{
       resolve("C")
     })
@@ -15,18 +15,16 @@ const getA = ()=>{
   
   const getABC = ()=>{
     return new Promise( async(resolve,reject)=>{
-      let arrFinal=[];
+      let arrFinal
   
       let soyA = getA()
-      let soyB; 
-      setImmediate(()=>{
-        getB(e=> console.log(e ))
-      })
+      let soyB = await getB(elemnt => elemnt); 
       let soyC = await getC()
       
+
+      arrFinal = [soyA, soyC]
       
-      
-      resolve(soyB)
+      resolve(arrFinal)
     })
   }
   
